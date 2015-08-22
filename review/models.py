3db16c9a154d57301b4684a68e965e894811941d
ThuleQaid <thuleqaid@163.com>
2015-08-23 07:38:08 +0800

    CheckGroup.details

--- review/models.py
+++ review/models.py
@@ -1,40 +1,97 @@
+import json
+import collections
+import logging
 from django.db import models
 from django.db.models import Max
 from django.contrib.auth.models import User
 from django.utils.encoding import python_2_unicode_compatible
 
+logger = logging.getLogger('review')
 # Create your models here.
+CONST_CODE_LEN = 63
 class BaseVersionTable(models.Model):
-	code        = models.CharField(max_length=20)
-	version     = models.PositiveIntegerField(default=1)
-	update_time = models.DateTimeField(auto_now_add=True)
-	def splitCode(self):
-		idx = self.code.index('#')
-		return self.code[0:idx], self.code[idx+1:]
-	def setCode(self,category,no):
-		self.code = '%s#%d'%(category,no)
-	def strPrefix(self):
-		return '[%d][%s][%d]'%(self.id,self.code,self.version)
-	@classmethod
-	def latest(cls, where=''):
-		return cls.objects.raw('SELECT *, Max(version) FROM %s %s GROUP BY code'%(cls.getTableName(), where))
-	@classmethod
-	def getTableName(cls):
-		appname = cls.__module__[0:cls.__module__.index('.')]
-		clsname = cls.__name__
-		tablename = '%s_%s'%(appname,clsname)
-		return tablename.lower()
-	class Meta:
-		abstract = True
-		#unique_together = [['code', 'version'],]
+    code        = models.CharField(max_length=CONST_CODE_LEN)
+    version     = models.PositiveIntegerField(default=1)
+    update_time = models.DateTimeField(auto_now_add=True)
+    def splitCode(self):
+        idx = self.code.index('#')
+        return self.code[0:idx], int(self.code[idx+1:])
+    def setCode(self,category,no):
+        self.code = '%s#%08d'%(category,no)
+    def strPrefix(self):
+        return '[%d][%s][%d]'%(self.id,self.code,self.version)
+    def isEqual(self, other, *excludeFields):
+        excludes = ('id','update_time')+excludeFields
+        same = True
+        for key in self.__dict__.keys():
+            if not key.startswith('_') and key not in excludes:
+                if self.__dict__[key] != other.__dict__.get(key,None):
+                    same = False
+                    break
+        return same
+    @classmethod
+    def latest(cls, where=''):
+        return cls.objects.raw('SELECT *, Max(version) FROM %s %s GROUP BY code'%(cls.getTableName(), where))
+    @classmethod
+    def getTableName(cls):
+        appname = cls.__module__[0:cls.__module__.index('.')]
+        clsname = cls.__name__
+        tablename = '%s_%s'%(appname,clsname)
+        return tablename.lower()
+    class Meta:
+        abstract = True
+
+@python_2_unicode_compatible
+class Project(BaseVersionTable):
+    #author = models.ForeignKey(User)
+    StatusOpen   = 'OP'
+    StatusClosed = 'ED'
+    StatusChoice = ((StatusOpen, 'Open'), (StatusClosed, 'Closed'),)
+    title = models.CharField(max_length=240)
+    status = models.CharField(max_length=2, choices=StatusChoice, default=StatusOpen)
+    def __str__(self):
+        return self.strPrefix()+self.title
+    class Meta:
+        unique_together = [['code', 'version'],]
+
+@python_2_unicode_compatible
+class CheckGroup(BaseVersionTable):
+    GroupDetailItem = collections.namedtuple('GroupDetailItem', ['valid','code','version'])
+    #author = models.ForeignKey(User)
+    project = models.CharField(max_length=CONST_CODE_LEN)
+    title = models.CharField(max_length=240)
+    details = models.TextField(default=json.dumps([]))
+    def contains(self, itemcode):
+        validlist = self.staticContains(self.details, itemcode)
+        listlen = len(validlist)
+        if listlen == 1:
+            return validlist[0]
+        elif listlen < 1:
+            return None
+        elif listlen > 1:
+            logger.error('CheckGroup[%s] contains multiple item[%s]'%(self.strPrefix(), itemcode))
+            return validlist[0]
+    @staticmethod
+    def staticContains(detailstr, itemcode):
+        return [x for x in unpackDetails(detailstr) if x.code == itemcode]
+    @staticmethod
+    def packDetails(detaillist):
+        return json.dumps(detaillist)
+    @staticmethod
+    def unpackDetails(detailstr):
+        return [GroupDetailItem(*x) for x in json.loads(detailstr)]
+    def __str__(self):
+        return self.strPrefix()+self.title
+    class Meta:
+        unique_together = [['code', 'version'],]
 
 @python_2_unicode_compatible
 class CheckItem(BaseVersionTable):
-	author = models.ForeignKey(User)
-	title = models.CharField(max_length=240)
-	details = models.TextField(default='')
-	def __str__(self):
-		return self.strPrefix()+self.title
-	class Meta:
-		unique_together = [['code', 'version'],]
+    #author = models.ForeignKey(User)
+    title = models.CharField(max_length=240)
+    details = models.TextField(default='')
+    def __str__(self):
+        return self.strPrefix()+self.title
+    class Meta:
+        unique_together = [['code', 'version'],]
 
--- review/views.py
+++ review/views.py
@@ -1,22 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import CheckItem
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.utils import timezone
+from .models import *
 
 # Create your views here.
 def index(request):
-	cis = CheckItem.latest()
-	return HttpResponse("<br />".join([str(x) for x in cis]))
+    cis = CheckItem.latest()
+    return HttpResponse("<br />".join([str(x) for x in cis]))
 
 def setup(request):
-	ci = CheckItem(code='AZ#0001', version=1, title='Check Item 11', author=request.user)
-	ci.save()
-	ci = CheckItem(code='AZ#0002', version=1, title='Check Item 21', author=request.user)
-	ci.save()
-	ci = CheckItem(code='AZ#0002', version=2, title='Check Item 22', author=request.user)
-	ci.save()
-	ci = CheckItem(code='AZ#0002', version=3, title='Check Item 23', author=request.user)
-	ci.save()
-	ci = CheckItem(code='AZ#0001', version=2, title='Check Item 12', author=request.user)
-	ci.save()
-	return index(request)
+    prj = Project(title='Project 1')
+    prj.setCode('Prj',123)
+    prj.version=1
+    prj.save()
+    grp = CheckGroup(project=prj.code, title='CheckGroup1')
+    grp.setCode('Grp',1)
+    grp.version=1
+    details=[]
+    itemcode='SS'
+    itemno=1
+    ci = CheckItem(version=1, title='Check Item 11')
+    ci.setCode(itemcode,itemno)
+    ci.save()
+    details.append(CheckGroup.GroupDetailItem(True,ci.code,ci.version))
+    ci = CheckItem(version=1, title='Check Item 21')
+    ci.setCode(itemcode,itemno+1)
+    ci.save()
+    details.append(CheckGroup.GroupDetailItem(True,ci.code,ci.version))
+    ci = CheckItem(version=2, title='Check Item 22')
+    ci.setCode(itemcode,itemno+1)
+    ci.save()
+    details.append(CheckGroup.GroupDetailItem(True,ci.code,ci.version))
+    ci = CheckItem(version=3, title='Check Item 23')
+    ci.setCode(itemcode,itemno+1)
+    ci.save()
+    details.append(CheckGroup.GroupDetailItem(True,ci.code,ci.version))
+    ci = CheckItem(version=2, title='Check Item 12')
+    ci.setCode(itemcode,itemno)
+    ci.save()
+    details.append(CheckGroup.GroupDetailItem(True,ci.code,ci.version))
+    grp.details=CheckGroup.packDetails(details)
+    grp.save()
+    return HttpResponseRedirect(reverse('review:index', args=()))
