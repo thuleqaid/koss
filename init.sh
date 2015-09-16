4e95aa77668c81c961727add96b74cebd769b330
ThuleQaid <thuleqaid@163.com>
2015-09-16 22:52:27 +0800

    navbar with login/logout link

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ rm -f db.sqlite3
 rm -f review/migrations/0*.*
 python manage.py makemigrations review
 python manage.py migrate
-#python manage.py createsuperuser --user admin --email admin@example.com
+python manage.py createsuperuser --user admin --email admin@example.com
 python manage.py runserver
